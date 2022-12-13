bcc83a31110049d0be6caab4e1a01e037ccf82e5
Chris Knudsvig <knudcb28@gmail.com>
2022-12-13 16:49:57 -0600

    added security update

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '579e60bf91744bccad5629a3f6f54ec2'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req, res, db) => {
   }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage,
+    handleApiCall
 }
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -22,20 +21,14 @@ const db = knex({
 const app = express();
 
 app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
-
-// Test only - when you have a database variable you want to use
-// app.get('/', (req, res)=> {
-//   res.send(database.users);
-// })
+app.use(express.json()); 
 
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
