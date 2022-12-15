53c95e2bb99fff593dfcc4495ee7f5e84ad3f1d4
Chris Knudsvig <knudcb28@gmail.com>
2022-12-15 08:35:01 -0600

    change

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
