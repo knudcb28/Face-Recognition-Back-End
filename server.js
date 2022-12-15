e03e04f14b037017967424cf7524b21ca84b8b36
Chris Knudsvig <knudcb28@gmail.com>
2022-12-15 12:32:58 -0600

    add

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const PORT = process.env.PORT || 3000;
 
 const db = knex({
   client: 'pg',
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (req, res) => { res.send('it is working!') })
+app.get('/', (req, res) => { res.send('db.users') })
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
@@ -29,6 +29,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(PORT, ()=> {
-  console.log("Ready on " + PORT);
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 });
