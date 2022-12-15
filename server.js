1313dbe7250ac2b0713dd97d91c14c600ff2705a
Chris Knudsvig <knudcb28@gmail.com>
2022-12-15 12:09:09 -0600

    adding database connection

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL, 
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: true, 
   }
 });
 
+
+
+
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (req, res) => { res.json('it is working!') })
+app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
@@ -30,6 +31,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running pon port ${process.env.PORT}`);
+app.listen(process.env.PORT, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
